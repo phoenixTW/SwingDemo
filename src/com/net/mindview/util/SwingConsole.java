0a8c7763fc7784f3798d29e240a72ee0f1b8ea6b
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-03-17 14:51:45 +0530

    new classes are created such as - Button1.java , Button2.java, SwingConsole.java.
    new packages are introduced - net.mindview.util, com.swingDemo

---
+++ src/com/net/mindview/util/SwingConsole.java
@@ -0,0 +1,20 @@
+package com.net.mindview.util;
+
+import javax.swing.*;
+
+/**
+ * Created by kaustavc on 3/17/2015.
+ */
+public class SwingConsole {
+    public static void run(final JFrame f, final int width, final int height) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                f.setTitle(f.getClass().getSimpleName());
+                f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                f.setSize(width, height);
+                f.setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/com/swingDemo/Button1.java
@@ -0,0 +1,22 @@
+package com.swingDemo;
+
+import javax.swing.*;
+import java.awt.*;
+import static com.net.mindview.util.SwingConsole.*;
+/**
+ * Created by kaustavc on 3/17/2015.
+ */
+public class Button1 extends JFrame {
+    private JButton
+        b1 = new JButton("Button 1"),
+        b2 = new JButton("Button 2");
+
+    public Button1() {
+        setLayout(new FlowLayout());
+        add(b1);
+        add(b2);
+    }
+    public static void main(String[] args) {
+        run(new Button1(), 200, 300);
+    }
+}
